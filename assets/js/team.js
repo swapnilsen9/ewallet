8274fdcd61d30888b20d647bd4f3789d37dbb01f
swapnilsen9 <60958846+swapnilsen9@users.noreply.github.com>
2021-05-07 23:38:25 +0530

    Update team.js

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,4 +1,4 @@
-const instanceUrl = "http://localhost:8000";
+const instanceUrl = "https://e-wallet-testing.herokuapp.com/";
 
 $(function () {
   let sessionUserName = sessionStorage.getItem("userName");
