632290f13d540fcd08d332e12e1e0429bc59d63e
swapnilsen9 <60958846+swapnilsen9@users.noreply.github.com>
2021-05-07 23:50:12 +0530

    Update register.js

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,4 +1,4 @@
-const instanceUrl = "https://e-wallet-testing.herokuapp.com/";
+const instanceUrl = "https://e-wallet-testing.herokuapp.com";
 
 $(function () {
   $("form[name='userForm']").validate({
